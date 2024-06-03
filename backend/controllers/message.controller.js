01d98ef5ac140be3e8371b6c79f31de65a274027
saiteja-in <vurukondasaiteja13@gmail.com>
2024-06-03 20:14:54 +0530

    initial comiit

---
+++ backend/controllers/message.controller.js
@@ -0,0 +1,31 @@
+import Conversation from "../models/conversation.model.js";
+import Message from "../models/message.model.js";
+export const  sendMessage=async(req,res)=>{
+    // console.log("message sent",req.params.id)
+    try {
+        const {message}=req.body;
+        const {id:receiverId}=req.params;  // or const id=req.params.id;
+        const senderId =req.user._id;
+
+        let conversation=await Conversation.findOne({
+            participants:{$all:[senderId,receiverId]}
+        })
+        if(!conversation){
+            conversation=await Conversation.create({
+                participants:[senderId,receiverId],
+            })
+        }
+        const newMessage=new Message({
+            senderId:senderId,
+            receiverId:receiverId,
+            message:message,
+        })
+        if(newMessage){
+            conversation.messages.push(newMessage._id)
+        }
+        res.status(201).json(newMessage)
+    } catch (error) {
+        console.log("error in sendmessage controller",error.message)
+        res.status(500).json({error:"internal service error"})
+    }
+}
---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import dotenv from "dotenv";
+import express from "express";
+import cookieParser from 'cookie-parser'
+import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDB from "./db/connectToMongoDB.js";
+import messageRoutes from './routes/message.routes.js'
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+dotenv.config();
+
+app.use(express.json())  //to parse incoming req from json payloads(from req.body)
+app.use(cookieParser)   //to read a cookie using req 
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+
+app.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`server is running at port ${PORT}`);
+});
