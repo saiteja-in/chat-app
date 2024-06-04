c27a9ab01b907f0e17dc2767079116d1025f0358
saiteja-in <vurukondasaiteja13@gmail.com>
2024-06-04 09:07:05 +0530

    get messages

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -37,3 +37,21 @@ export const  sendMessage=async(req,res)=>{
         res.status(500).json({error:"internal service error"})
     }
 }
+
+export const getMessages=async(req,res)=>{
+    try {
+        const{id:userToChatId}=req.params;
+        const senderId=req.user._id;
+        const conversation = await Conversation.findOne({
+            participants:{$all:[senderId,userToChatId]}
+        }).populate("messages"); //not reference but actual msgs
+//it doesnot give the messages array in the conversation but total message (id,senderid,receiverid,message array)
+        if(!conversation) return res.status(200).json([])
+        const messages=conversation.messages;
+        res.status(200).json(messages)
+
+    } catch (error) {
+        console.log("error in getMessages controller",error.message)
+        res.status(500).json({error:"internal service error"})
+    }
+}
