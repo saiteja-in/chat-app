4943efaf14b62381e218f3134ad9c0c1c2e8b126
saiteja-in <vurukondasaiteja13@gmail.com>
2024-06-05 23:51:57 +0530

    context api and logout functionality

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-import cors from "cors";
+// import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import cookieParser from 'cookie-parser'
@@ -8,7 +8,7 @@ import messageRoutes from './routes/message.routes.js'
 import userRoutes from './routes/user.route.js'
 const app = express();
 const PORT = process.env.PORT || 5000;
-app.use(cors());
+// app.use(cors());
 dotenv.config();
 
 app.use(express.json())  //to parse incoming req from json payloads(from req.body)
