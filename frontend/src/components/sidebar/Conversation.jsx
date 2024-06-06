3612c423f838bd17f02fc5879576d5935e4ef0fa
saiteja-in <vurukondasaiteja13@gmail.com>
2024-06-06 18:33:40 +0530

    all done except socket io integration

--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
+import useConversation from '../../zustand/useConversation';
 
-const Conversation = () => {
+const Conversation = ({conversation,lastIdx}) => {
+
+	const{selectedConversation,setSelectedConversation}=useConversation()
+	const isSelected=selectedConversation?._id===conversation._id
 	return (
 		<>
-			<div className='flex gap-2 items-center text-gray-500 hover:text-blue-500 transform hover:scale-105 transition duration-300 hover:bg-pink-500 rounded p-2 py-1 cursor-pointer'>
+			<div className={`flex gap-2 items-center text-gray-500 hover:bg-pink-500 transform hover:scale-105 transition duration-300 ${isSelected?"bg-pink-500":""} rounded p-2 py-1 cursor-pointer`}
+			onClick={()=>setSelectedConversation(conversation)}>
 				<div className='avatar online'>
 					<div className='w-12 rounded-full'>
 						<img
-							src='https://cdn0.iconfinder.com/data/icons/communication-line-10/24/account_profile_user_contact_person_avatar_placeholder-512.png'
+							src={conversation.profilePic}
 							alt='user avatar'
 						/>
 					</div>
@@ -15,13 +20,13 @@ const Conversation = () => {
 
 				<div className='flex flex-col flex-1'>
 					<div className='flex gap-3 justify-between'>
-						<p className='font-bold text-gray-200'>John Doe</p>
-						<span className='text-xl'>🎃</span>
+						<p className='font-bold text-gray-200'>{conversation.fullName}</p>
+						{/* <span className='text-xl'>🎃</span> */}
 					</div>
 				</div>
 			</div>
 
-			<div className='divider my-0 py-0 h-1' />
+			{!lastIdx && <div className='divider my-0 py-0 h-1' />}
 		</>
 	);
 };
