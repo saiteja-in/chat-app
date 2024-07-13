e1bd1bd20c2c415756f6ee1ac517d50acc894a06
saiteja-in <vurukondasaiteja13@gmail.com>
2024-07-14 01:05:45 +0530

    minor updates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,39 @@
-import React from 'react'
-import Login from './pages/login/Login'
-import SignUp from './pages/signup/SignUp'
-import Home from './pages/home/Home'
-import { Navigate, Route, Routes } from 'react-router-dom'
-import { Toaster } from 'react-hot-toast'
-import { useAuthContext } from './context/AuthContext'
+import React from "react";
+import Login from "./pages/login/Login";
+import SignUp from "./pages/signup/SignUp";
+import Home from "./pages/home/Home";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import { useAuthContext } from "./context/AuthContext";
 
 const App = () => {
-  const{authUser}=useAuthContext()
+  const { authUser } = useAuthContext();
   return (
-    <div className='p-4 h-screen flex items-center justify-center'>
-    <Routes>
-      <Route path='/' element={authUser ? <Home />: <Navigate to={"/login"}/>} />
-      <Route path='/signup' element={authUser ? <Navigate to='/'/> : <SignUp />} />
-      <Route path='/login' element={authUser ? <Navigate to='/'/> : <Login />} />
-    </Routes>
-    <Toaster />
-    </div>
-  )
-}
+    <>
+      <div class="relative h-full w-full bg-slate-950">
+        <div class="absolute bottom-0 left-[-20%] right-0 top-[-10%] min-h-screen min-w-screen rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
+        <div className="p-4 min-h-screen flex items-center justify-center">
+        <Routes>
+          <Route
+            path="/"
+            element={authUser ? <Home /> : <Navigate to={"/login"} />}
+          />
+          <Route
+            path="/signup"
+            element={authUser ? <Navigate to="/" /> : <SignUp />}
+          />
+          <Route
+            path="/login"
+            element={authUser ? <Navigate to="/" /> : <Login />}
+          />
+        </Routes>
+        <Toaster />
+      </div>
+        <div class="absolute bottom-0 right-[-20%] top-[-10%] min-h-screen min-w-screen rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
+      </div>
+      
+    </>
+  );
+};
 
-export default App
+export default App;
