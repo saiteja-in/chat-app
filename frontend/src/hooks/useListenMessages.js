df6af7c7e0bc1a4f984caae7b7259ef007e9e6cf
saiteja-in <vurukondasaiteja13@gmail.com>
2024-08-13 17:19:35 +0530

    socket implementation and notification sound added

--- frontend/src/hooks/useListenMessages.js
+++ frontend/src/hooks/useListenMessages.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import { useEffect } from "react";
+
+import { useSocketContext } from "../context/SocketContext";
+import useConversation from "../zustand/useConversation";
+
+import notificationSound from "../../public/sounds/mixkit-correct-answer-tone-2870.wav";
 
 const useListenMessages = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+	const { socket } = useSocketContext();
+	const { messages, setMessages } = useConversation();
 
-export default useListenMessages
+	useEffect(() => {
+		socket?.on("newMessage", (newMessage) => {
+			newMessage.shouldShake = true;
+			const sound = new Audio(notificationSound);
+			sound.play();
+			setMessages([...messages, newMessage]);
+		});
+
+		return () => socket?.off("newMessage");
+	}, [socket, setMessages, messages]);
+};
+export default useListenMessages;
