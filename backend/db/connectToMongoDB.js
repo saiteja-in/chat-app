5486ecb4dd82ca84c4219e010088e2892f61020e
saiteja-in <vurukondasaiteja13@gmail.com>
2025-07-19 15:37:01 +0530

    fix the connection issues

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -1,11 +1,12 @@
-import mongoose from "mongoose"
-const connectToMongoDB=async()=>{
-    try{
-        await mongoose.connect(process.env.MONGO_DB_URI);
-        console.log("connected to mongodb")
-    }catch(error){
-        console.log("error connecting to mongodb")
-    }
-}
+import mongoose from "mongoose";
+
+const connectToMongoDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_DB_URI);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log("Error connecting to MongoDB:", error.message);
+  }
+};
 
 export default connectToMongoDB;
--- backend/server.js
+++ backend/server.js
@@ -1,35 +1,34 @@
-// import cors from "cors";
-import path from "path"
 import dotenv from "dotenv";
+dotenv.config();
+
+// import cors from "cors";
+import path from "path";
 import express from "express";
-import cookieParser from 'cookie-parser'
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
-import messageRoutes from './routes/message.routes.js'
-import userRoutes from './routes/user.route.js'
-import { app,server } from "./socket/socket.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.route.js";
+import { app, server } from "./socket/socket.js";
 // const app = express();
 const PORT = process.env.PORT || 5000;
 
-const __dirname=path.resolve()
+const __dirname = path.resolve();
 // app.use(cors());
-dotenv.config();
 
-app.use(express.json())  //to parse incoming req from json payloads(from req.body)
-app.use(cookieParser())   //to read a cookie using req 
+app.use(express.json()); //to parse incoming req from json payloads(from req.body)
+app.use(cookieParser()); //to read a cookie using req
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-
-app.use(express.static(path.join(__dirname,"/frontend/dist")))
-app.use("*",(req,res)=>{
-  res.sendFile(path.join(__dirname,'frontend','dist','index.html'))
-})
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.use("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 app.get("/health", (req, res) => {
   res.send("health is good");
 });
-
 
 server.listen(PORT, () => {
   connectToMongoDB();
